d98ab3ce0a0a40b5e88253f541047e7b72c97e58
Kyle E. Mitchell <kyle@kemitchell.com>
2019-07-01 23:31:38 -0700

    Test with Node.js assert

---
+++ test.js
@@ -0,0 +1,9 @@
+var assert = require('assert')
+var lib = require('./');
+
+var password = 'password';
+lib.hash(password, function(error, digest) {
+  lib.check(password, digest, function(error, result) {
+    assert(result === true);
+  });
+});
